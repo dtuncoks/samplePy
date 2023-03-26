6841482c6ae6ac84c44110e35c3d7e1092ee0345
dtuncoks <96452436+dtuncoks@users.noreply.github.com>
2023-03-26 11:23:49 -0400

    Add files via upload

---
+++ samplePy.py
@@ -0,0 +1,26 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2708
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 fname = "Dotun"\
+lname = "Coker"\
+age = 32\
+\
+\
+print(fname + " " + lname)\
+print("My first name is " + fname + " and my last name is " + lname)\
+print(1990)\
+print(age)\
+#print days\
+Days = ["Monday", 'Tuesday', "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]\
+\
+print(Days[4])\
+\
+birth_year = input('Enter youe Birth Year: ')\
+year = 2022\
+age = int(year) - int(birth_year)\
+\
+print(age)}
